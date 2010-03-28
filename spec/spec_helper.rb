2e87ac830eede155a46a6d7a4505a3fee93aadd2
Josh Martin <jmartin@webwideconsulting.com>
2010-03-28 16:44:36 -0700

    Initial Prerelease

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,9 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
+require 'rubygems'
 require 'mongo'
-require 'mongo_queue'
+require 'lib/mongo_queue'
 
 Spec::Runner.configure do |config|
   #MongoQueue.log.level = Logger::FATAL
